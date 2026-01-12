3c85111b48fd98c836bd5ba16cc75883d8fe2b09
battlmonstr <battlmonstr@users.noreply.github.com>
2026-01-12 15:51:05 +0100

    decode claimAsset parameters

--- src/claim_endpoint.rs
+++ src/claim_endpoint.rs
@@ -1,10 +1,15 @@
 use crate::service_state::ServiceState;
 use alloy::consensus::TxEnvelope;
 use alloy::eips::Decodable2718;
+use alloy_core::sol_types::SolCall;
 use axum::Json;
 use axum::extract::State;
 use hex::FromHexError;
 use serde::{Deserialize, Serialize};
+
+fn hex_decode_prefixed(input: &str) -> Result<Vec<u8>, FromHexError> {
+    hex::decode(input.strip_prefix("0x").unwrap_or(input))
+}
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -18,18 +23,42 @@ pub struct ClaimRequest {
 #[serde(rename_all = "camelCase")]
 pub struct ClaimResponse {}
 
+alloy_core::sol! {
+    // https://github.com/agglayer/agglayer-contracts/blob/main/contracts/v2/PolygonZkEVMBridgeV2.sol#L556
+    #[derive(Debug)]
+    function claimAsset(
+        bytes32[32] calldata smtProofLocalExitRoot,
+        bytes32[32] calldata smtProofRollupExitRoot,
+        uint256 globalIndex,
+        bytes32 mainnetExitRoot,
+        bytes32 rollupExitRoot,
+        uint32 originNetwork,
+        address originTokenAddress,
+        uint32 destinationNetwork,
+        address destinationAddress,
+        uint256 amount,
+        bytes calldata metadata
+    );
+}
+
 pub async fn claim_endpoint_dry_run(
     State(_service): State<ServiceState>,
     Json(request): Json<ClaimRequest>,
 ) -> Json<ClaimResponse> {
     tracing::debug!("chain_id: {:?}", request.chain_id);
     tracing::debug!("to: {:?}", request.to);
-    tracing::debug!("input: {:?}", request.input);
-    Json(ClaimResponse {})
-}
 
-fn hex_decode_prefixed(input: &str) -> Result<Vec<u8>, FromHexError> {
-    hex::decode(input.strip_prefix("0x").unwrap_or(input))
+    let Ok(params_encoded) = hex_decode_prefixed(&request.input) else {
+        return Json(ClaimResponse {});
+    };
+    if params_encoded.starts_with(&claimAssetCall::SELECTOR) {
+        let params = claimAssetCall::abi_decode(&params_encoded);
+        tracing::debug!("claimAsset call params: {:?}", params);
+    } else {
+        panic!("unhandled txn method {:?}", params_encoded);
+    }
+
+    Json(ClaimResponse {})
 }
 
 pub async fn claim_endpoint_raw_txn(
@@ -46,6 +75,14 @@ pub async fn claim_endpoint_raw_txn(
             let txn = txn_signed.tx();
             tracing::debug!("chain_id: {:?}", txn.chain_id);
             tracing::debug!("to: {:?}", txn.to);
+
+            let params_encoded = &txn.input;
+            if params_encoded.starts_with(&claimAssetCall::SELECTOR) {
+                let params = claimAssetCall::abi_decode(params_encoded)?;
+                tracing::debug!("claimAsset call params: {:?}", params);
+            } else {
+                panic!("unhandled txn method {:?}", params_encoded);
+            }
         },
         _ => {
             panic!("unhandled txn type {:?}", txn_envelope.tx_type());
